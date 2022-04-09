f9a60d2f11432642c2a2bcdabfd74762b57aad6f
Wirat Sakorn <sunneed.2555@gmail.com>
2022-04-09 11:48:13 +0700

    delete update read

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,15 @@ var router = express.Router();
 const db = require('monk')('localhost:27017/nodejs_mongo');
 const {check, validationResult} = require('express-validator');
 
-/* GET product listing. */
+// get all product
 router.get('/', function(req, res, next) {
-    res.render('product');
+    var collection = db.get('product');
+    collection.aggregate([{$sort: {'created_date': -1}}]).then((products, error) => {
+        res.render('product', {products: products});
+    })
 });
 
-/* GET add product. */
+// show add_product page
 router.get('/add', function(req, res, next) {
     res.render('add_product');
 });
@@ -22,26 +25,56 @@ router.post('/add', [
         res.render('add_product', {errors: errors});
     } else {
         var collection = db.get('product');
+        const created_date = new Date();
         collection.insert({
             name: req.body.name,
-            description: req.body.description
+            description: req.body.description,
+            created_date: created_date
         }, function(err, product){
             if(err) {
                 res.send(err);
             } else {
                 req.flash('error', 'Add product successfully');
-                res.location('/product/add');
-                res.redirect('/product/add');
+                res.location('/product');
+                res.redirect('/product');
             }
         });
     }
 });
 
-router.get('/edit', function(req, res, next) {
-    res.send('edit the product');
+router.get('/edit/:id', function(req, res, next) {
+    var collection = db.get('product');
+    collection.findOne({_id: req.params.id}).then((product, error) => {
+        res.render('edit_product', {product: product});
+    });
 })
-router.get('/delete', function(req, res, next) {
-    res.send('delete the product');
+
+router.post('/edit', [
+    check('name', 'Please input product name').not().isEmpty(),
+    check('description', 'Please input description').not().isEmpty()
+], function(req, res, next) {
+    const result = validationResult(req);
+    let errors = result.errors;
+    if (!result.isEmpty()) {
+        req.flash('error', 'Update failed');
+        res.redirect('/product/edit/' + req.body._id);
+        
+    } else {
+        var collection = db.get('product');
+        console.log(req.body);
+        collection.update({_id: req.body._id}, {$set: {name: req.body.name, description: req.body.description}}).then(() => {
+            req.flash('error', 'Update product successfully');
+            res.redirect('/product');
+        });
+    }
+});
+
+router.get('/delete/:id', function(req, res, next) {
+    var collection = db.get('product');
+    collection.findOneAndDelete({_id: req.params.id}).then(() => {
+        res.location('/product');
+        res.redirect('/product');
+    })
 })
 
 module.exports = router;
